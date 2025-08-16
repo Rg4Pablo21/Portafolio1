94b82b42bdbc1c59c8f6cf456da648a89375de31
RG4Pablo21 <gic438@gmail.com>
2025-08-15 19:37:02 -0600

    Terminado el Portafolio

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from './components/app.js';
-import { projects } from '../data/projects.js';
-
-// Crear la aplicación
-const app = document.getElementById('app');
-app.appendChild(createApp(projects));
